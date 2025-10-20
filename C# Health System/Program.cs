38273609a1e320eca3477a7282bab2e141f79d13
Kazooie56 <w0465155@campus.nscc.ca>
2025-10-20 10:34:55 -0300

    Final Update,
    no more debug messages
    
    shorter showcase

--- C# Health System/Program.cs
+++ C# Health System/Program.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Reflection.Emit;
 using System.Security.Cryptography;
 using System.Text;
@@ -23,70 +24,36 @@ namespace C__Health_System
         static int xp = 0;
         static int level = 1;
 
-
-
         static void Main(string[] args)
         {
             UnitTestHealthSystem();
             UnitTestXPSystem();
 
-            ////Showcasing ShowHud() 
+            Console.Clear();
+            ResetGame();                // Showcasing ResetGame() working
             ShowHUD();                  // working
-            ResetGame();                // Showcasing ResetGame() working, although it doesnt fix anything yet, we reuse it a ton in the code.
 
+            TakeDamage(50);
+            ShowHUD();
+            TakeDamage(150);
+            ShowHUD();
+            Revive();
+            ShowHUD();
+            TakeDamage(150);
+            ShowHUD();
+            Heal(50);
+            ShowHUD();
+            RegenerateShield(50);
+            ShowHUD();
+            IncreaseXP(100);
+            ShowHUD();
+            IncreaseXP(200);
+            ShowHUD();
 
-            ////Showcasing Heal()
-            health = 1;                 // health is being set to 1 for demonstration.
-            Heal(-10);                  // error message as intended for values less than 1.
-            Heal(50);                   // health goes up by 50
-            Heal(150);                  // healing over 100 without going over maximum
-
-            ////Showcasing RegenerateShield()
-            ResetGame();
-            shield = 1;                 // shield is being set to 1 for demonstration.
-            RegenerateShield(-10);      // error message as intended for values less than 1.
-            RegenerateShield(50);       // shield goes up by 50
-            RegenerateShield(150);      // shield increased by over 100 without going over maximum
-
-            ////Showcasing TakeDamage() with -10 damage not working, then taking 50 damage to shields first, then 100 damage to show the damage transfers over correctly from shield to health, then showing that death doesn't automatically respawn you.
-            ResetGame();
-            TakeDamage(-10);            // error message as intended for values less than 1.
-            TakeDamage(50);             // deals 50 damage to shields first
-            TakeDamage(100);            // deals 50 damage to shields first then the remaining damage is added to health.
-            TakeDamage(300);            // On brightspace you say revive() should be called when you die but in class you said you should not have it call when you have 0 health left because it breaks the code we needed to run it through. That's why my code doesn't do that.
-
-            ////Showcasing Revive()
-            ResetGame();
-            shield = 0; health = 1; lives = 1;  // This is to make him have try to revive with health remaining, which will fail
-            Revive();                           // showcasing a failed revive while alive
-            health = 0;                         // This is to make him dead so we can use revive
-            Revive();                           // a successful revive
-            health = 0;                         // this is to show what happens if you try and revive with no lives left.
-            Revive();                           // a failed revive while dead
-
-            //Showcasing IncreaseXP()   
-            ResetGame();
-            IncreaseXP(-10);                // error message as intended for values less than 1
-            IncreaseXP(100);                // shows 100 being the first level up breakpoint
-            IncreaseXP(100);                // shows how it increased by 100 since last level
-            IncreaseXP(100);                // shows that 200 is the correct amount of how much it needed to be increased
-            IncreaseXP(700);                // adding 700 and getting exactly level 5 proves the pattern will continue
+            // show taking damage, taking a ton of damage, show revive, show regenhealth, shields, and xp.
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-            
         }
         // v NO CHANGING ANYTHING IN HERE v
         static void UnitTestHealthSystem()
@@ -313,7 +280,6 @@ namespace C__Health_System
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("Error detected: cant gain less than 1 experience.\n");
                 Console.ResetColor();
-                ShowHUD();
                 return;
             }
 
@@ -324,18 +290,18 @@ namespace C__Health_System
                 xp -= level * 100; // subtract XP needed for this level
                 level++;            // increase level
             }
-
-            ShowHUD();
         }
 
         static void TakeDamage(int damage)
         {
+
+
+
             if (damage < 0)
             {
-                Console.ForegroundColor = ConsoleColor.Red;                         //I fought for three hours trying to make this work so the console would show up below. I don't know why I did that.
-                Console.WriteLine("Error detected: Damage can't be negative.\n");
-                Console.ResetColor();
-                ShowHUD();
+                //Console.ForegroundColor = ConsoleColor.Red;                         //I fought for three hours trying to make this work so the console would show up below. I don't know why I did that.
+                //Console.WriteLine("Error detected: Damage can't be negative.\n");
+                //Console.ResetColor();
                 return;
             }
 
@@ -347,51 +313,43 @@ namespace C__Health_System
                 shield = 0;
                 health -= piercingDamage;
             }
-            if (health < 0) { health = 0; }
-
-            ShowHUD();                  // NEEDS TO BE CALLED LAST OTHERWISE YOU START DEALING 90 DAMAGE AT THE BEGINNING OF UNITTESTHEALTHSYSTEM() FOR NO REASON - 12:05 AM
+            if (health < 0) { health = 0; }     // I wrote this back when I called ShowHUD() in all my methods. // NEEDS TO BE CALLED LAST OTHERWISE YOU START DEALING 90 DAMAGE AT THE BEGINNING OF UNITTESTHEALTHSYSTEM() FOR NO REASON - 12:05 AM            
         }
         static void Heal(int hp)
         {
             if (hp < 0)
             {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Error detected: can't heal less than 1 health.\n");      
-                Console.ResetColor();
-                ShowHUD();
+                //Console.ForegroundColor = ConsoleColor.Red;
+                //Console.WriteLine("Error detected: can't heal less than 1 health.\n");      
+                //Console.ResetColor();
                 return;
             }
 
             health += hp;
 
             if (health > 100) health = 100;
-
-            ShowHUD();
         }
         static void RegenerateShield(int hp)
         {
             if (hp < 0)
             {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Error detected: can't regenerate less than 1 shield.\n");
-                Console.ResetColor();
-                ShowHUD(); return;
+                //Console.ForegroundColor = ConsoleColor.Red;
+                //Console.WriteLine("Error detected: can't regenerate less than 1 shield.\n");
+                //Console.ResetColor();
+                return;
             }
 
             shield += hp;
 
             if (shield > 100) { shield = 100; }
-
-            ShowHUD();
         }
         static void Revive()
         {
             if(health >= 1)
             {
-                Console.ForegroundColor = ConsoleColor.Red;                         
+                Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("Error detected: you cant revive a alive guy.\n");
                 Console.ResetColor();
-                ShowHUD();
                 return;
             }
             else if (lives < 1)
@@ -399,7 +357,6 @@ namespace C__Health_System
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("Error detected: You have no more lives left.\n");
                 Console.ResetColor();
-                ShowHUD();
                 return;
             }
             health = 100;
@@ -426,54 +383,68 @@ namespace C__Health_System
     }
 }
 
-//C#: Health System:
-
-//Code a health system (and an xp system) and showcase that it works.
-
-//Extra Mile - XP/Level Up System - Technical Specifications:
-
-//methods:
-//IncreaseXP(int exp) method
-// modifies xp and level
-//leveling up uses up xp -- this make it easier to code (new!)
-//ShowHUD() method extended
-//shows previous stats (see health system for reference)
-//xp/level design:
-//same design as before, only now costs xp to level up -- easier to code (new!)
-//start at 0 xp
-//start at level 1
-//at level 1, costs 100 xp to level up to level 2
-//at level 2, costs 200 xp to level up to level 3
-//at level 3, costs 300 xp to level up to level 4
-//etc.
-//no hard coding
-//Feel free to add more extra mile, including more specs that do not interfere with given tech specs. In addition, not instead of.
-//Tips:
-
-//Overwhelmed? Divide and conquer.
-//ResetGame() method
-//resets all variables to default values
-//useful for testing
-//evolve methods such as ShowHUD() showing changes such as +5 and -10 after the values
-//Purpose:
-
-//All minimal requirements must be met, else work is unaccepted and evaluates to zero (0).
-//Submission Requirements:
-
-//Submit the following:
-
-//build
-//GitHub link
-//Testing Code Example:
-
-//Test and showcase your code works:
-
-
-
-
 // https://nscconline.brightspace.com/d2l/lms/dropbox/user/folder_submit_files.d2l?db=583995&grpid=0&isprv=0&bp=0&ou=356685
 
 // Old Code----------------------------------------------------------------------------
+
+// ////Showcasing Heal()
+//ShowHUD();
+//Console.WriteLine("health is being set to 1 for demonstration.");
+//Console.WriteLine("error message as intended for values less than 1.");
+//health = 1;
+//Heal(-10);
+//ShowHUD();
+//Console.WriteLine("health goes up by 50");
+//Heal(50);                   // 
+//ShowHUD();
+//Heal(150);                  // healing over 100 without going over maximum
+//ShowHUD();
+
+//////Showcasing RegenerateShield()
+//ResetGame();
+//shield = 1;                 // shield is being set to 1 for demonstration.
+//RegenerateShield(-10);      // error message as intended for values less than 1.
+//ShowHUD();
+//RegenerateShield(50);       // shield goes up by 50
+//ShowHUD();
+//RegenerateShield(150);      // shield increased by over 100 without going over maximum
+//ShowHUD();
+
+//////Showcasing TakeDamage() with -10 damage not working, then taking 50 damage to shields first, then 100 damage to show the damage transfers over correctly from shield to health, then showing that death doesn't automatically respawn you.
+//ResetGame();
+//TakeDamage(-10);            // error message as intended for values less than 1.
+//ShowHUD();
+//TakeDamage(50);             // deals 50 damage to shields first
+//ShowHUD();
+//TakeDamage(100);            // deals 50 damage to shields first then the remaining damage is added to health.
+//ShowHUD();
+//TakeDamage(300);            // On brightspace you say revive() should be called when you die but in class you said you should not have it call when you have 0 health left because it breaks the code we needed to run it through. That's why my code doesn't do that.
+//ShowHUD();
+
+//////Showcasing Revive()
+//ResetGame();
+//shield = 0; health = 1; lives = 1;  // This is to make him have try to revive with health remaining, which will fail
+//Revive();                           // showcasing a failed revive while alive
+//ShowHUD();
+//health = 0;                         // This is to make him dead so we can use revive
+//Revive();                           // a successful revive
+//ShowHUD();
+//health = 0;                         // this is to show what happens if you try and revive with no lives left.
+//Revive();                           // a failed revive while dead
+//ShowHUD();
+
+////Showcasing IncreaseXP()   
+//ResetGame();
+//IncreaseXP(-10);                // error message as intended for values less than 1
+//ShowHUD();
+//IncreaseXP(100);                // shows 100 being the first level up breakpoint
+//ShowHUD();
+//IncreaseXP(100);                // shows how it increased by 100 since last level
+//ShowHUD();
+//IncreaseXP(100);                // shows that 200 is the correct amount of how much it needed to be increased
+//ShowHUD();
+//IncreaseXP(700);                // adding 700 and getting exactly level 5 proves the pattern will continue
+//ShowHUD();
 
 //switch (health)
 //{
